ef75cbb9e3869a76160349f699c0408d82971e24
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-05-06 07:54:59 +0800

    somethingsomething

--- app/components/forms/auth/LoginForm.tsx
+++ app/components/forms/auth/LoginForm.tsx
@@ -4,6 +4,13 @@ import { Label } from "~/components/ui/label";
 import { Form } from "react-router";
 import { useNavigation } from "react-router";
 import type { AuthFieldErrors } from "~/types/auth";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "~/components/ui/card";
 
 interface LoginFormProps {
   fieldErrors: AuthFieldErrors;
@@ -14,31 +21,41 @@ export function LoginForm({ fieldErrors }: LoginFormProps) {
   const isLoading = navigation.state === "loading";
   return (
     <div>
-      <Form method="post">
-        <div className="space-y-1">
-          <div>
-            <Label htmlFor="username">Username</Label>
-            <Input type="text" name="username" />
-            {fieldErrors?.username._errors.map((err, i) => (
-              <p key={i} className="text-red-500 text-sm">
-                {err}
-              </p>
-            ))}
-          </div>
-          <div>
-            <Label htmlFor="password">Password</Label>
-            <Input type="password" name="password" />
-            {fieldErrors?.password._errors.map((err, i) => (
-              <p key={i} className="text-red-500 text-sm">
-                {err}
-              </p>
-            ))}
-          </div>
-          <Button disabled={isLoading} className="w-full" type="submit">
-            {isLoading ? "Logging in... " : "Login"}
-          </Button>
-        </div>
-      </Form>
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your credentials to access your account
+          </CardDescription>
+        </CardHeader>
+        <Form method="post">
+          <CardContent>
+            <div className="space-y-1">
+              <div>
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" name="username" />
+                {fieldErrors?.username._errors.map((err, i) => (
+                  <p key={i} className="text-red-500 text-sm">
+                    {err}
+                  </p>
+                ))}
+              </div>
+              <div>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" name="password" />
+                {fieldErrors?.password._errors.map((err, i) => (
+                  <p key={i} className="text-red-500 text-sm">
+                    {err}
+                  </p>
+                ))}
+              </div>
+              <Button disabled={isLoading} className="w-full" type="submit">
+                {isLoading ? "Logging in... " : "Login"}
+              </Button>
+            </div>
+          </CardContent>
+        </Form>
+      </Card>
     </div>
   );
 }
